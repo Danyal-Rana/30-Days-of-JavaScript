b810bc497c75080082f9c10ec6c04320fad85b26
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-02-16 23:29:23 +0500

    Time: 54 ms (49.96%), Space: 49.1 MB (8.35%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    
+    let temp = true;
+    
+    return function(...args){
+        
+        if (temp) {
+            temp = false;
+            return fn (...args);
+        }
+        
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
